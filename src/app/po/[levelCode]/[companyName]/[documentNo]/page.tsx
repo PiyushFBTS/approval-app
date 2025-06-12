d8bb843ff9fd6609c566085791616cb5749d38a2
PiyushFBTS <piyush.malviya@fbts.in>
2025-06-12 13:00:51 +0530

    fix:build

--- src/app/po/[levelCode]/[companyName]/[documentNo]/page.tsx
+++ src/app/po/[levelCode]/[companyName]/[documentNo]/page.tsx
@@ -1,14 +1,18 @@
 import PoViewerClient from "@/components/PoViewerClient";
 
-export default function Page({ params }: {
-  params: { levelCode: string; companyName: string; documentNo: string };
+export default async function Page({ params }: {
+  params: Promise<{ levelCode: string; companyName: string; documentNo: string }>
 }) {
+  
+  const levelCode = (await params)?.levelCode
+  const companyName = (await params)?.companyName
+  const documentNo = (await params)?.documentNo
+
   const decodedParams = {
-    levelCode: decodeURIComponent(params.levelCode),
-    companyName: decodeURIComponent(params.companyName),
-    documentNo: decodeURIComponent(params.documentNo),
+    levelCode: decodeURIComponent(levelCode),
+    companyName: decodeURIComponent(companyName),
+    documentNo: decodeURIComponent(documentNo),
   };
 
   return <PoViewerClient params={decodedParams} />;
 }
-
